c03cb9896a940c7b5fc77ed983b1076bb51e23c9
Yash Jangir <60678034+offjangir@users.noreply.github.com>
2020-10-03 15:37:00 +0530

    Update uart_drive_rosserial.ino

--- drive/uart_drive/uart_drive_rosserial.ino
+++ drive/uart_drive/uart_drive_rosserial.ino
@@ -1,10 +1,12 @@
 #include <ros.h>
-#include <SoftwareSerial.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 #define baudrate 115200
 #define vmax 0.22
 #define wmax 2.84
+#define MDDS1Serial Serial1
+#define MDDS2Serial Serial2
+#define MDDS2Serial Serial3
 
 ros::NodeHandle nh; //Node decleration
 
@@ -19,13 +21,7 @@ float ang_vel; //omega mapped down to ang_vel
 int rxPin =0;
 int txPin = 1;
 float vel_wheels[6];
-int serial_pins[3] = {8,9,10};
 float motorLspeed1,motorLspeed2,motorLspeed3,motorRspeed1,motorRspeed2,motorRspeed3;
-
-
-SoftwareSerial MDDS1Serial=SoftwareSerial(rxPin,serial_pins[0]);
-SoftwareSerial MDDS2Serial=SoftwareSerial(rxPin,serial_pins[1]);
-SoftwareSerial MDDS3Serial=SoftwareSerial(rxPin,serial_pins[2]);
 
 void sub_cb(const geometry_msgs::Twist &twist_obj)
 {
@@ -50,11 +46,6 @@ void setup() {
     nh.initNode();
     nh.subscribe(cmd_vel_sub);
     nh.advertise(chatter);
- 
-      for ( int i=0; i<3 ;i++)
-    {
-        pinMode(serial_pins[i],OUTPUT);
-    }
        
        MDDS1Serial.begin(baudrate);
        MDDS2Serial.begin(baudrate);
